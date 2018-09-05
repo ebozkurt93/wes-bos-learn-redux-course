8010ec8e3c39611a1cecd124f9848ad5af68ff7d
Erdem Bozkurt <ebozkurt@sabanciuniv.edu>
2018-09-05 14:37:37 +0300

    video 8

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -14,7 +14,7 @@ import PhotoGrid from './components/PhotoGrid';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
-    <Router history = {browserHistory}>
+  <Router history={browserHistory}>
         <Route path="/" component={Main}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
         <Route path="/view/:postId" component={Single}></Route>
